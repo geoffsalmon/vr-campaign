4ef1a8ff9f40a15005aeb1505fc07a0cfda7d5ae
stuart spence <stuart.a.spence@gmail.com>
2016-03-19 17:22:25 -0400

    removed radius of attraction, now global, added fps debug script

--- Assets/Scripts/Marine Life/FishSchool.cs
+++ Assets/Scripts/Marine Life/FishSchool.cs
@@ -7,10 +7,9 @@ public class FishSchool : MonoBehaviour
 	public int fishCount = 200;
 	public float interval = 0.5f; //this is how often each fish will re-orient itself
 	public float baseSpeed = 4; //fish swim speed
-
+	
 	//for more info on radii and weights see this academic paper:
 	//"Simulating The Collective Behavior of Schooling Fish With A Discrete Stochastic Model" 2006
-	public float radiusOfAttraction = 150;
 	public float radiusOfOrientation = 60;
 	public float radiusOfRepulsion = 4;
 	public float weightOfRepulsion = 1.5f;
@@ -18,16 +17,17 @@ public class FishSchool : MonoBehaviour
 	public float weightOfAttraction = 1.5f;
 	public float weightOfSelf = 5;
 	private float weightOfOutOfBounds; //this is used when the fish goes below the floor, or above the water
-
+	private Vector3 averageFishPosition;
+	
 	public GameObject maxHeightGameObject;
 	public Terrain minHeightTerrain;
 	private float schoolWidth = 50; //octree initialization size, small optimization consideration
 	private Fish[] fishies;
 	private GameObject fishContainer;
 	private Bounds fishBounds;
-	private Bounds boundsOfAttraction, boundsOfOrientation, boundsOfRepulsion;
+	private Bounds boundsOfOrientation, boundsOfRepulsion;
 	private BoundsOctree<Fish> octree;
-
+	
 	void Start ()
 	{
 		if (prefab == null) {
@@ -40,15 +40,16 @@ public class FishSchool : MonoBehaviour
 			Debug.LogError ("maxHeightGameObject is null. no max height!");
 		if (minHeightTerrain == null)
 			Debug.LogError ("minHeightTerrain is null. no min height!");
-
+		
+		StartCoroutine (CalculateAverageFishPosition ());
 		weightOfOutOfBounds = (weightOfSelf + weightOfRepulsion + weightOfOrientation + weightOfAttraction)/5;
 		SetupFishies ();
 		MakeFishies ();
-
+		
 		if (interval > 1)
 			Debug.LogError ("Warning: interval greater than one. Might cause weirdness in ApplyZones.");
 	}
-
+	
 	private void SetupFishies ()
 	{
 		fishies = new Fish[fishCount];
@@ -56,11 +57,10 @@ public class FishSchool : MonoBehaviour
 		fishBounds = new Bounds (Vector3.zero, new Vector3 (1, 1, 1)); //fish are 1x1x1 meters, only needed for octree, values don't matter much
 		octree = new BoundsOctree<Fish> (schoolWidth, transform.position, 0.1f, 1);
 
-		boundsOfAttraction = new Bounds (Vector3.zero, new Vector3 (radiusOfAttraction, radiusOfAttraction, radiusOfAttraction));
 		boundsOfOrientation = new Bounds (Vector3.zero, new Vector3 (radiusOfOrientation, radiusOfOrientation, radiusOfOrientation));
 		boundsOfRepulsion = new Bounds (Vector3.zero, new Vector3 (radiusOfRepulsion, radiusOfRepulsion, radiusOfRepulsion));
 	}
-
+	
 	private void MakeFishies ()
 	{		
 		float halfWidth = schoolWidth / 2;
@@ -75,13 +75,23 @@ public class FishSchool : MonoBehaviour
 			StartCoroutine (Cycle (fishies [i]));
 		}
 	}
-
+	
 	void Update ()
 	{
 		foreach (Fish fish in fishies)
 			fish.Swim (interval);
 	}
-
+	
+	private IEnumerator CalculateAverageFishPosition(){
+		while (true) {
+			averageFishPosition=Vector3.zero;
+			foreach(Fish fish in fishies)
+				averageFishPosition+=fish.gameObject.transform.position;
+			averageFishPosition/=fishies.Length;
+			yield return new WaitForSeconds(interval);
+		}
+	}
+	
 	private IEnumerator Cycle (Fish fish)
 	{
 		yield return new WaitForSeconds (Random.Range (0, interval)); //this initial wait is to spread out fish computation
@@ -92,17 +102,19 @@ public class FishSchool : MonoBehaviour
 			
 		}
 	}
-
+	
 	private void ApplyZones (Fish fish)
 	{
 		//calculate and apply the new orientation this fish should have
 		Vector3 selfDirection = fish.gameObject.transform.forward * weightOfSelf;
 		Vector3 repulsion = GetNearbyFishVector (fish, boundsOfRepulsion) * weightOfRepulsion; //get the vector that best faces away from very nearby fish
 		Vector3 orientation = GetNearbyFishDirection (fish, boundsOfOrientation) * weightOfOrientation; //get the direction that nearby fish are generally facing
-		Vector3 attraction = GetNearbyFishVector (fish, boundsOfAttraction) * weightOfAttraction; //get the unit vector that best faces towards all fish except those very far away
+		
+		Vector3 attractionDirection = averageFishPosition - fish.gameObject.transform.position;
+		Vector3 attraction = attractionDirection.normalized * weightOfAttraction; //get the unit vector that best faces towards all fish except those very far away
 		
 		Vector3 idealDirection = selfDirection - repulsion + orientation + attraction;
-
+		
 		//if fish is above water or below ground, turn it up/down
 		if (maxHeightGameObject != null && fish.gameObject.transform.position.y > GetMaxHeight ()) {
 			idealDirection += Vector3.down * weightOfOutOfBounds;
@@ -114,15 +126,15 @@ public class FishSchool : MonoBehaviour
 		Vector3 newDirection = Vector3.Lerp (fish.gameObject.transform.forward, idealDirection, interval); //go more towards this new direction if interval is high and the fish check less often
 		fish.SetNewFacingDirection (newDirection);
 	}
-
+	
 	private float GetMaxHeight(){
 		return maxHeightGameObject.transform.position.y-5;
 	}
-
+	
 	private float GetMinHeight(Fish fish){
 		return minHeightTerrain.SampleHeight (fish.gameObject.transform.position) + minHeightTerrain.transform.position.y + 5;
 	}
-
+	
 	private Vector3 GetNearbyFishDirection (Fish fish, Bounds bounds)
 	{
 		//looks at all fish surrounding fish within bounds bounds, and returns the average direction they're facing
@@ -132,7 +144,7 @@ public class FishSchool : MonoBehaviour
 			direction += otherFish.gameObject.transform.forward;
 		return direction.normalized;
 	}
-
+	
 	private Vector3 GetNearbyFishVector (Fish fish, Bounds bounds)
 	{
 		//looks at all fish surrounding fish within bounds bounds, and returns a unit vector averaging their positions relative to fish
@@ -142,7 +154,7 @@ public class FishSchool : MonoBehaviour
 			nearby += otherFish.gameObject.transform.position - fish.gameObject.transform.position;
 		return nearby.normalized;
 	}
-
+	
 	private void UpdateOctree (Fish fish)
 	{
 		//octree is a data structure to efficiently keep track of which fishies are near other fishies. But it must be updated regularly.
