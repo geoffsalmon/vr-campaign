04d5416038d796fee32470deb04f7ec0c5425369
Geoff Salmon <geoff.salmon@gmail.com>
2016-03-29 10:38:40 -0400

    Adds curved tunnels and movement between reefs
    The movement prior to entering the tunnel and after exiting the tunnel
    is still a straight lerp, so the transition into and out of the tunnel
    is still jarring.

---
+++ Assets/Scripts/VisitReefs/Transits/CubicBezier.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubicBezier {
+	public Vector3 p0 { get; private set; }
+	public Vector3 p1 { get; private set; }
+	public Vector3 p2 { get; private set; }
+	public Vector3 p3 { get; private set; }
+
+	public CubicBezier(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
+		this.p0 = p0;
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+	}
+
+	public Vector3 position(float t) {
+		float u = 1 - t;
+		return u * u * u * p0 + 3 * u * u * t * p1 + 3 * u * t * t * p2 + t * t * t * p3;
+	}
+
+	public Vector3 derivative(float t) {
+		float u = 1 - t;
+		return 3 * u * u * (p1 - p0) + 6 * u * t * (p2 - p1) + 3 * t * t * (p3 - p2);
+	}
+
+	public Vector3 second_derivative(float t) {
+		float u = 1 - t;
+		return 6 * u * (p2 - 2*p1 + p0) + 6 * t * (p3 - 2*p2 + p1);
+	}
+
+	/*public void curve(float t, out Vector3 position, out Vector3 derivative) {
+		float u = 1 - t;
+		derivative = 3 * u * u * (p1 - p0) + 6 * u * t * (p2 - p1) + 3 * t * t * (p3 - p2);
+	}
+	\mathbf{B}(t)=(1-t)^3\mathbf{P}_0+3(1-t)^2t\mathbf{P}_1+3(1-t)t^2\mathbf{P}_2+t^3\mathbf{P}_3 \mbox{ , } 0 \le t \le 1.
+		For some choices of P1 and P2 the curve may intersect itself, or contain a cusp.
+
+			Any series of any 4 distinct points can be converted to a cubic Bézier curve that goes through all 4 points in order. Given the starting and ending point of some cubic Bézier curve, and the points along the curve corresponding to t = 1/3 and t = 2/3, the control points for the original Bézier curve can be recovered.[5]
+
+			The derivative of the cubic Bézier curve with respect to t is
+
+				\mathbf{B}'(t) = 3(1-t)^2(\mathbf{P}_1 - \mathbf{P}_0) + 6(1-t)t(\mathbf{P}_2 - \mathbf{P}_1) + 3t^2(\mathbf{P}_3 - \mathbf{P}_2) \,.
+The second derivative of the Bézier curve with respect to t is
+
+\mathbf{B}''(t) = 6(1-t)(\mathbf{P}_2 - 2 \mathbf{P}_1 + \mathbf{P}_0) +  6t(\mathbf{P}_3 - 2 \mathbf{P}_2 + \mathbf{P}_1) \,.
+*/
+
+}
---
+++ Assets/Scripts/VisitReefs/Transits/CurvedTunnel.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CurvedTunnel : MonoBehaviour {
+	public Material TubeMaterial;
+
+	private GameObject tube;
+	private bool tubeAdded = false;
+
+	void OnEnable() {
+		Transit.OnTransit += OnTransit;	
+	}
+
+	void OnDisable() {
+		Transit.OnTransit -= OnTransit;
+	}
+
+	private float calcRadius(float t) {
+		return 8 * (t - 0.5f) * (t - 0.5f) + 1;
+	}
+
+	private void OnTransit(Transit transit, Transit.TransitState state, float progress) {
+		if (state == Transit.TransitState.Entering && !tubeAdded) {
+			tubeAdded = true;
+
+			GameObject tube = new GameObject("tube");
+			MeshFilter filter = tube.AddComponent<MeshFilter>();
+
+			List<TubePoint> points = new List<TubePoint>();
+			CubicBezier curve = transit.Curve;
+
+			for (float t = 0f; t < 1f; t += 0.2f) {
+				points.Add(new TubePoint(curve.position(t), curve.derivative(t), calcRadius(t)));
+			}
+			points.Add(new TubePoint(curve.position(1), curve.derivative(1), calcRadius(1)));
+
+			/*foreach (TubePoint p in points) {
+				Debug.Log(p.position + " " + p.derivative + " " + p.radius);
+			}*/
+
+			TubeMesh.generateTubes(filter.mesh, points, 8);
+
+			MeshRenderer renderer = tube.AddComponent<MeshRenderer>();
+			renderer.receiveShadows = false;
+			renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+			if (TubeMaterial == null) {
+				renderer.material.color = Color.red;
+			} else {
+				renderer.material = TubeMaterial;
+			}
+			tube.transform.parent = gameObject.transform;
+		} else if (state == Transit.TransitState.Finished) {
+			// will be destroyed automatically at end
+		}
+	}
+}
