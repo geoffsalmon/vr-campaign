7aa3a56a37b6e913b79820d753a66d607356c158
Peter Wilkinson <peterwilkinson@qq.com>
2016-03-29 00:05:39 -0400

    saving before major update from master

---
+++ Assets/PlaytestDev/Scripts/StartScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+	public GameObject spawnObject;
+	// Use this for initialization
+	void Start () {
+		GameObject clone;
+    	clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(-90,0,0)) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
