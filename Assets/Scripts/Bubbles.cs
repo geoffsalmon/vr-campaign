422bbb46c03d206f7bd59ce7792c111b975cbabb
Peter Wilkinson <peterwilkinson@qq.com>
2016-03-30 05:28:03 -0400

    test check
    bubbles was used to drive 1280 clones in a test. with some new
    Vector3(Random.Range(-100,100),0,0) added and an 80 , 600, 600 clone
    added to each loop respectively.

--- Assets/Scripts/Bubbles.cs
+++ Assets/Scripts/Bubbles.cs
@@ -7,15 +7,16 @@ public class Bubbles : MonoBehaviour {
 	void Start () {
 		for(int i=0;i<8;i++){
 			GameObject clone;
+			// Testing load + new Vector3(Random.Range(-100.0F,100.0F),0,0)
     		clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(0,45*i,0)) as GameObject;
 		}
 		for(int i=0;i<6;i++){
 			GameObject clone;
-    		clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(-45,60*i,0)) as GameObject;
+			clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(-45,60*i,0)) as GameObject;
 		}
 		for(int i=0;i<6;i++){
 			GameObject clone;
-    		clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(45,60*i,0)) as GameObject;
+			clone = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(45,60*i,0)) as GameObject;
 		}
 		GameObject cloneTop;
     		cloneTop = Instantiate(spawnObject.transform, transform.position, Quaternion.Euler(-90,0,0)) as GameObject;
