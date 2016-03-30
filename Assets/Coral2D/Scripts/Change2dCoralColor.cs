1d5de6cd86ee1350f999085effe3fda1ea172b29
Peter Wilkinson <peterwilkinson@qq.com>
2016-03-29 23:50:40 -0400

    Coral2D package made and deployed
    In order to keep organized, I packaged the 2D coral, which makes it
    easier to give to other people and load in any Unity project. It’s self
    contained and updating is just releasing another package. … its just a
    thought since some of the assets are getting further and further apart
    from each other in the project.

---
+++ Assets/Coral2D/Scripts/Change2dCoralColor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Change2dCoralColor : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		Color newColor = new Color( Random.value, Random.value, Random.value, 1.0f );
+		// = newColor;
+		//renderer.material.color = newColor;
+		Debug.Log(newColor);
+		GetComponent<MeshRenderer>().material.color = newColor;
+//		Color myColor;
+//bool mode = Random.value > 0.5f;
+//float rand = Random.value;
+//int index = Random.Range( 0, 3 );
+//myColor[index] = rand;
+//myColor[( index + ( mode ? 1 : 2 ) ) % 3] = 1;
+//myColor[( index + ( mode ? 2 : 1 ) ) % 3] = 0;
+//renderer.material.color = myColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
