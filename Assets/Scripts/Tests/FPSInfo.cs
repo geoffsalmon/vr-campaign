bf0943e72fa8872fa7bf803c46d9dd373fdfd19d
Stuart Spence <stuart.a.spence@gmail.com>
2016-03-31 00:09:09 -0400

    added options of FPS info

--- Assets/Scripts/Tests/FPSInfo.cs
+++ Assets/Scripts/Tests/FPSInfo.cs
@@ -3,17 +3,18 @@ using System.Collections;
 using UnityEngine.UI;
 
 //attach this script, once, to any gameobject and there will be a debug canvas with an FPS counter, updated every second
-public class FPSInfo : MonoBehaviour {
+public class FPSInfo : MonoBehaviour
+{
 	public bool showOnScreen = true;
 	public bool showInConsole = true;
 	private GameObject canvas;
 	private Text text;
-
-	private float interval=1;
-	private float timer=0;
+	private float interval = 1;
+	private float timer = 0;
 	private int frameCount;
 
-	void Start () {
+	void Start ()
+	{
 		if (showOnScreen) {
 			GameObject prefab = Resources.Load ("test/Debug Canvas") as GameObject;
 			canvas = Instantiate (prefab) as GameObject;
@@ -22,18 +23,19 @@ public class FPSInfo : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		frameCount++;
 		timer += Time.deltaTime;
 		if (timer > interval) {
-			float fps=Mathf.Round((float)frameCount/interval);
-			timer=0;
-			frameCount=0;
+			float fps = Mathf.Round ((float)frameCount / interval);
+			timer = 0;
+			frameCount = 0;
 
-			if(showOnScreen)
-			text.text=fps+" FPS";
-			if(showInConsole)
-			Debug.Log (fps+" FPS");
+			if (showOnScreen)
+				text.text = fps + " FPS";
+			if (showInConsole)
+				Debug.Log (fps + " FPS");
 		}
 	}
 }
